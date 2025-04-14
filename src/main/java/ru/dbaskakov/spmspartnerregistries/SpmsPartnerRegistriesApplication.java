9b3395e880ac49eb1876435d46379faabacfaed7
r1no933 <dbaskakov93@yandex.ru>
2025-04-14 11:18:21 +0300

    init project

---
+++ src/main/java/ru/dbaskakov/spmspartnerregistries/SpmsPartnerRegistriesApplication.java
@@ -0,0 +1,13 @@
+package ru.dbaskakov.spmspartnerregistries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmsPartnerRegistriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpmsPartnerRegistriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dbaskakov/spmspartnerregistries/SpmsPartnerRegistriesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dbaskakov.spmspartnerregistries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmsPartnerRegistriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
